42a68e9b26b34cae449554ff48115699d712b53b
aolivas92 <86969842+aolivas92@users.noreply.github.com>
2024-02-21 06:15:41 +0000

    added import for java.io.File

--- evaluation/subjects/Eclipse_jetty_1_eps1_1/src/Driver_Greedy.java
+++ evaluation/subjects/Eclipse_jetty_1_eps1_1/src/Driver_Greedy.java
@@ -14,6 +14,7 @@ import edu.cmu.sv.kelinci.quantification.PartitionSet;
 import edu.cmu.sv.kelinci.quantification.Greedy;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
