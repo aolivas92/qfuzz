0a3b58cb061c15c50914c2fe7d46e71cceae34ac
aolivas92 <olivas9215@hotmail.com>
2024-02-23 16:44:00 -0700

    Updated logging paths and removed unused imports

--- evaluation/subjects/leaksn1b-5_1/src/Driver_Greedy.java
+++ evaluation/subjects/leaksn1b-5_1/src/Driver_Greedy.java
@@ -17,177 +17,171 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.lang.NumberFormatException;
 import java.io.PrintWriter;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
-
 public class Driver_Greedy {
 
-	/* Maximum number of different observations. */
-	public final static int K = 2;
+  /* Maximum number of different observations. */
+  public final static int K = 2;
 
-	/* Minimum distance between clusters. */
-	public final static double epsilon = 1.0;
-	
-	/* Cluster Algorithm */
-	public static PartitionAlgorithm clusterAlgorithm = new Greedy(false);
+  /* Minimum distance between clusters. */
+  public final static double epsilon = 1.0;
 
-	////////////////////////////////////////////////////////////////////////
+  /* Cluster Algorithm */
+  public static PartitionAlgorithm clusterAlgorithm = new Greedy(false);
 
-	public static void main(String[] args) {
+  ////////////////////////////////////////////////////////////////////////
 
-		if (args.length != 1) {
-			System.out.println("Expects file name as parameter");
-			return;
-		}
+  public static void main(String[] args) {
 
-		int public_guess;
-		int[] secrets = new int[K];
+    if (args.length != 1) {
+      System.out.println("Expects file name as parameter");
+      return;
+    }
 
-		// Read all inputs.
-		try (FileInputStream fis = new FileInputStream(args[0])) {
-			byte[] bytes = new byte[Integer.BYTES];
+    int public_guess;
+    int[] secrets = new int[K];
 
-			if (fis.read(bytes) < 0) {
-				throw new RuntimeException("Not enough input data...");
-			}
-			public_guess = Math.floorMod(ByteBuffer.wrap(bytes).getInt(),(int) Math.pow(2,30));
+    // Read all inputs.
+    try (FileInputStream fis = new FileInputStream(args[0])) {
+      byte[] bytes = new byte[Integer.BYTES];
 
-			for (int i = 0; i < secrets.length; i++) {
-				if (fis.read(bytes) < 0) {
-					throw new RuntimeException("Not enough input data...");
-				}
-				secrets[i] = Math.floorMod(ByteBuffer.wrap(bytes).getInt(),(int) Math.pow(2,28));
-			}
+      if (fis.read(bytes) < 0) {
+        throw new RuntimeException("Not enough input data...");
+      }
+      public_guess = Math.floorMod(ByteBuffer.wrap(bytes).getInt(), (int) Math.pow(2, 30));
 
-		} catch (IOException e) {
-			System.err.println("Error reading input");
-			e.printStackTrace();
-			return;
-		}
+      for (int i = 0; i < secrets.length; i++) {
+        if (fis.read(bytes) < 0) {
+          throw new RuntimeException("Not enough input data...");
+        }
+        secrets[i] = Math.floorMod(ByteBuffer.wrap(bytes).getInt(), (int) Math.pow(2, 28));
+      }
 
-		System.out.println("public_guess=" + public_guess);
-		for (int i = 0; i < secrets.length; i++) {
-			System.out.println("secret" + i + "=" + secrets[i]);
-		}
+    } catch (IOException e) {
+      System.err.println("Error reading input");
+      e.printStackTrace();
+      return;
+    }
 
-		long[] observations = new long[K];
-		Mem.clear(true);
-		for (int i = 0; i < K; i++) {
-			Mem.clear(false);
-			leaks_n1s.leaks_n1s(secrets[i], public_guess);
-			observations[i] = Mem.instrCost;
-		}
-		System.out.println("observations: " + Arrays.toString(observations));
+    System.out.println("public_guess=" + public_guess);
+    for (int i = 0; i < secrets.length; i++) {
+      System.out.println("secret" + i + "=" + secrets[i]);
+    }
 
-		PartitionSet clusters = PartitionSet.createFromObservations(epsilon, observations, clusterAlgorithm);
-		Kelinci.setObserverdClusters(clusters.getClusterAverageValues(), clusters.getMinimumDeltaValue());
+    long[] observations = new long[K];
+    Mem.clear(true);
+    for (int i = 0; i < K; i++) {
+      Mem.clear(false);
+      leaks_n1s.leaks_n1s(secrets[i], public_guess);
+      observations[i] = Mem.instrCost;
+    }
+    System.out.println("observations: " + Arrays.toString(observations));
 
-		// Calculate analytics, Nathan
+    PartitionSet clusters = PartitionSet.createFromObservations(epsilon, observations, clusterAlgorithm);
+    Kelinci.setObserverdClusters(clusters.getClusterAverageValues(), clusters.getMinimumDeltaValue());
+
+    // Calculate analytics, Nathan
     double analytics = Math.abs(observations[0] - observations[1]);
 
     // Log Everything, Alex
+    String logPath = "./log/Log.txt";
     String data = Double.toString(analytics);
-    appendToLog("log.txt", data);
+    appendToLog(logPath, data);
 
     // Read Everything from Unique file, Alex
-    SortedSet<Double> uniqueValues = readDoubleSetLog("Unique_Log.txt");
+    String uniqueLogPath = "./log/Unique_Log.txt";
+    SortedSet<Double> uniqueValues = readDoubleSetLog(uniqueLogPath);
 
-    if(!uniqueValues.contains(analytics))
-    {
-        uniqueValues.add(analytics);
-        appendToLog("Unique_Log.txt", Double.toString(analytics));
+    if (!uniqueValues.contains(analytics)) {
+      uniqueValues.add(analytics);
+      appendToLog(uniqueLogPath, Double.toString(analytics));
     }
 
-    // Size threshold -- should be set to 10, but for this subject there is only 3 unique values
-    int threshold = 10;
+    // Size threshold -- should be set to 10, but for this subject there is only 3
+    // unique values
+    int threshold = 2;
 
-    if(uniqueValues.size() > threshold) {
-        if (expTest(uniqueValues,threshold)) {
-            appendToLog("log.txt", "exp test passed");
-        }
+    if (uniqueValues.size() > threshold) {
+      if (expTest(uniqueValues, threshold)) {
+        appendToLog(logPath, "-1");
+      }
     }
 
     System.out.println("Done.");
   }
 
+  /* Logging Algorithm */
+  public static void appendToLog(String fileName, String data) {
+    try {
+      FileWriter writer = new FileWriter(fileName, true);
+      PrintWriter out = new PrintWriter(writer);
 
-    /* Logging Algorithm */
-    public static void appendToLog(String fileName, String data) {
-      try {
-        FileWriter writer = new FileWriter(fileName, true);
-        PrintWriter out = new PrintWriter(writer);
+      out.println(data);
 
-        out.println(data);
-        
-        out.close();
-        writer.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+      out.close();
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    /* Read Log File */
-    public static SortedSet<Double> readDoubleSetLog(String fileName) {
-      SortedSet<Double> doubleSet = new TreeSet<>();
+  /* Read Log File */
+  public static SortedSet<Double> readDoubleSetLog(String fileName) {
+    SortedSet<Double> doubleSet = new TreeSet<>();
 
-      File file = new File(fileName);
+    File file = new File(fileName);
 
-      try {
-          // If file doesn't exists, create one and return empty set
-          if (!file.exists()) {
-              file.createNewFile();
-              return doubleSet;
-          }
+    try {
+      // If file doesn't exists, create one and return empty set
+      if (!file.exists()) {
+        file.createNewFile();
+        return doubleSet;
+      }
 
-          // Read file if it exists
-          BufferedReader reader = new BufferedReader(new FileReader(fileName));
-          String line;
-          while ((line = reader.readLine()) != null) {
-              try {
-                double value = Double.parseDouble(line.trim());
-                doubleSet.add(value);
-              } catch (NumberFormatException e) {
-                e.printStackTrace();
-              }
-          }
-
-          reader.close();
-      } catch (IOException e) {
+      // Read file if it exists
+      BufferedReader reader = new BufferedReader(new FileReader(fileName));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        try {
+          double value = Double.parseDouble(line.trim());
+          doubleSet.add(value);
+        } catch (NumberFormatException e) {
           e.printStackTrace();
+        }
       }
 
-      return doubleSet;
+      reader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    public static boolean expTest(SortedSet<Double> arr,int threshold)
-    {
-        double sum = 0;
-        double mean = 0;
-        double standardDeviation = 0;
-        int i = 0;
-        for (double item : arr)
-        {
-            if(i > threshold) {
-                break;
-            }
-            sum += item;
-            i++;
-        }
-        mean = sum / i;
-        int j = 0;
 
-        for (double item: arr)
-        {
-            if(j > threshold) {
-                break;
-            }
-            standardDeviation += Math.sqrt(Math.pow(item - mean, 2));
-            j++;
-        }
-        return standardDeviation / mean > 1;
+    return doubleSet;
+  }
+
+  public static boolean expTest(SortedSet<Double> arr, int threshold) {
+    double sum = 0;
+    double mean = 0;
+    double standardDeviation = 0;
+    int i = 0;
+    for (double item : arr) {
+      if (i > threshold) {
+        break;
+      }
+      sum += item;
+      i++;
     }
+    mean = sum / i;
+    int j = 0;
+
+    for (double item : arr) {
+      if (j > threshold) {
+        break;
+      }
+      standardDeviation += Math.sqrt(Math.pow(item - mean, 2));
+      j++;
+    }
+    return standardDeviation / mean > 1;
+  }
 }
