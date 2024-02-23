a14ed2c0d58e84d23f5eba0f23d090556c2af155
aolivas92 <86969842+aolivas92@users.noreply.github.com>
2024-02-23 19:01:36 +0000

    commented out everything unused

--- evaluation/subjects/Eclipse_jetty_1_eps1_1/src/calculateStandardDeviation.java
+++ evaluation/subjects/Eclipse_jetty_1_eps1_1/src/calculateStandardDeviation.java
@@ -1,25 +1,25 @@
 
-public class calculateStandardDeviation {
-    public static void calculateStandardDeviation() {
-    }
+// public class calculateStandardDeviation {
+//     public static void calculateStandardDeviation() {
+//     }
 
-    public double[] calculateStandardDeviation(long[] array) {
-        // get the sum of array
-        long sum = 0;
-        for (long i : array) {
-            sum += i;
-        }
+//     public double[] calculateStandardDeviation(long[] array) {
+//         // get the sum of array
+//         long sum = 0;
+//         for (long i : array) {
+//             sum += i;
+//         }
 
-        // get the mean of array
-        int length = array.length;
-        double mean = (double) sum / length;
+//         // get the mean of array
+//         int length = array.length;
+//         double mean = (double) sum / length;
 
-        // calculate the standard deviation
-        double standardDeviation = 0.0;
-        for (long num : array) {
-            standardDeviation += Math.pow(num - mean, 2);
-        }
+//         // calculate the standard deviation
+//         double standardDeviation = 0.0;
+//         for (long num : array) {
+//             standardDeviation += Math.pow(num - mean, 2);
+//         }
 
-        return 1.0;
-    }
-}
+//         return 1.0;
+//     }
+// }
