7ba13787180d4b3ce9d042c5c814cc7081cfb197
aolivas92 <olivas9215@hotmail.com>
2024-02-29 17:02:17 -0700

    updated min_num_tail checker

--- evaluation/subjects/Eclipse_jetty_1_eps1_1/src/Driver_Greedy.java
+++ evaluation/subjects/Eclipse_jetty_1_eps1_1/src/Driver_Greedy.java
@@ -142,7 +142,7 @@ public class Driver_Greedy {
     int min_num_tail = 15;
     int threshold = 10;
 
-    if (uniqueValues.size() > min_num_tail) {
+    if (uniqueValues.size() >= min_num_tail) {
       if (expTest(threshold, uniqueValues)) {
         testPassed = true;
         locationPassed = count;
