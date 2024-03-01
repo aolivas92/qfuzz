9657fa8bda1da9c6b41f246f4daf7b1494ecf5fa
aolivas92 <olivas9215@hotmail.com>
2024-03-01 11:50:51 -0700

    updated exp test checker

--- evaluation/subjects/leaksn1b-5_1/src/Driver_Greedy.java
+++ evaluation/subjects/leaksn1b-5_1/src/Driver_Greedy.java
@@ -112,7 +112,7 @@ public class Driver_Greedy {
     int min_num_tail = 15;
     int threshold = 10;
 
-    if (!testPassed && uniqueValues.size() >= min_num_tail) {
+    if (testPassed == false && uniqueValues.size() >= min_num_tail) {
       if (expTest(threshold, uniqueValues)) {
         testPassed = true;
         locationPassed = count;
