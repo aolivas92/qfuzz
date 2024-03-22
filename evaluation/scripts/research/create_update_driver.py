cdb48a10c4fe93220dcb67c21be050cd0ac95137
aolivas92 <olivas9215@hotmail.com>
2024-03-22 15:31:57 -0600

    Updated script to work more dynamically with imports

--- evaluation/scripts/research/create_update_driver.py
+++ evaluation/scripts/research/create_update_driver.py
@@ -1,8 +1,8 @@
 import os
 
 subjects=[
-#  "Eclipse_jetty_1_eps1_1",
- "leaksn1b-5_1",
+ "Eclipse_jetty_1_eps1_1",
+#  "leaksn1b-5_1",
 #  "blazer_loopandbranch_unsafe",
 #  "blazer_modpow1_unsafe",
 #  "blazer_modpow2_unsafe",
@@ -11,29 +11,30 @@ subjects=[
 #  "blazer_login_unsafe",
 ]
 
-source_file = "../../subjects/Eclipse_jetty_1_eps1_1/src/Driver_Greedy.java"
+source_file = "../../subjects/leaksn1b-5_1/src/Driver_Greedy_Guarantee.java"
 start_string = "// Start of research"
 
-destination_code_start_string_1 = "// Calculate analytics, Nathan"
+destination_code_start_string_1 = "// Start of research"
 destination_code_start_string_2 = 'System.out.println("Done.");'
-
-update_imports_flag = True
 
 destination_imports_start_string_1 = "import java.util.Arrays;"
 destination_imports_start_string_2 = "import edu.cmu.sv.kelinci.quantification.Greedy;"
 
-new_imports_1 = ["import java.util.ArrayList;\n",
+new_imports_1 = {"import java.io.FileInputStream;\n",
+                 "import java.io.IOException;\n",
+                 "import java.nio.ByteBuffer;\n",
+                 "import java.util.Arrays;\n",
+                 "import java.util.ArrayList;\n",
                  "import java.util.List;\n",
                  "import java.util.TreeSet;\n",
                  "import java.util.stream.Stream;\n",
-                 "import java.util.SortedSet;\n",]
-new_imports_2 = ["\n", 
-                 "import java.io.BufferedReader;\n",
+                 "import java.util.SortedSet;\n",}
+new_imports_2 = {"import java.io.BufferedReader;\n",
                  "import java.io.File;\n",
                  "import java.io.FileReader;\n",
                  "import java.io.FileWriter;\n",
                  "import java.lang.NumberFormatException;\n",
-                 "import java.io.PrintWriter;\n",]
+                 "import java.io.PrintWriter;\n",}
 
 with open(source_file, 'r') as source:
         source_lines = source.readlines()
@@ -57,6 +58,8 @@ for subject in subjects:
         os.system('sed -i "s|public class Driver_Greedy {|public class Driver_Greedy_Guarantee {|g" "' + new_driver + '"')
 
     destination_file = "../../subjects/" + subject + "/src/Driver_Greedy_Guarantee.java"
+    needed_imports_1 = set(new_imports_1).copy()
+    needed_imports_2 = set(new_imports_2).copy()
 
     with open(destination_file, 'r+') as destination:
         destination_lines = destination.readlines()
@@ -65,10 +68,16 @@ for subject in subjects:
         insertion_imports_1_index = -1
         insertion_imports_2_index = -1
         for i, line in enumerate(destination_lines):
-            if update_imports_flag and destination_imports_start_string_1 in line:
+            if destination_imports_start_string_1 in line:
                 insertion_imports_1_index = i
-            if update_imports_flag and destination_imports_start_string_2 in line:
+            if line in needed_imports_1:
+                needed_imports_1.remove(line)
+
+            if destination_imports_start_string_2 in line:
                 insertion_imports_2_index = i
+            if line in needed_imports_2:
+                needed_imports_2.remove(line)
+
             if destination_code_start_string_1 in line or destination_code_start_string_2 in line:
                 insertion_code_index = i
                 break
@@ -77,20 +86,21 @@ for subject in subjects:
             destination_lines[insertion_code_index:] = extracted_code
         else:
             print("Failed code insertion with: " + subject)
-            continue
 
-        if update_imports_flag and insertion_imports_1_index != -1:
-            destination_lines[:] = destination_lines[:insertion_imports_1_index + 1] + new_imports_1 + destination_lines[insertion_imports_1_index + 1:]
+
+        if insertion_imports_1_index != -1 and len(needed_imports_1) > 0:
+            destination_lines[:] = destination_lines[:insertion_imports_1_index + 1] + list(needed_imports_1) + destination_lines[insertion_imports_1_index + 1:]
         else:
             print("Failed imports 1 with: " + subject)
-            continue
         
-        insertion_imports_2_index += len(new_imports_1)
-        if update_imports_flag and insertion_imports_2_index != -1:
-            destination_lines[:] = destination_lines[:insertion_imports_2_index + 1] + new_imports_2 + destination_lines[insertion_imports_2_index + 1:]
+        insertion_imports_2_index += len(needed_imports_1)
+        if insertion_imports_2_index != -1 and len(needed_imports_2) > 0:
+            destination_lines[:] = destination_lines[:insertion_imports_2_index + 1] + ["\n"] + list(needed_imports_2) + destination_lines[insertion_imports_2_index + 1:]
         else:
             print("Failed imports 2 with: " + subject)
-            continue
         
         destination.seek(0)  # Rewind to the beginning of the destination file
         destination.writelines(destination_lines)
+    
+    cur_subject = os.path.join("../../subjects/" + subject + "/src/Driver_Greedy_Guarantee.java")
+    # os.system('git add ' + cur_subject)
