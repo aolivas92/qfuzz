cf1e76e4a1b7882201b51b86ea7a7fcbdf85e07d
aolivas92 <olivas9215@hotmail.com>
2024-03-23 17:33:53 -0600

    Update dir log path

--- evaluation/subjects/blazer_login_unsafe/src/Driver_Greedy_Guarantee.java
+++ evaluation/subjects/blazer_login_unsafe/src/Driver_Greedy_Guarantee.java
@@ -109,7 +109,7 @@ public class Driver_Greedy_Guarantee {
     // Calculate analytics, Nathan
     double analytics = Math.abs(observations[0] - observations[1]);
 
-    String dirPath = "./log/log_30min_10/";
+    String dirPath = "./log/log_30min_2/";
 
     // Log Everything, Alex
     String logPath = "Log.txt";
