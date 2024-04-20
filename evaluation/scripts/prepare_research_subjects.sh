c6b631fa5e9b2b84dfc7892e22685e35a68fd0de
aolivas92 <olivas9215@hotmail.com>
2024-04-20 10:51:20 -0600

    Updated subjects

--- evaluation/scripts/prepare_research_subjects.sh
+++ evaluation/scripts/prepare_research_subjects.sh
@@ -1,17 +1,15 @@
 trap "exit" INT
 
 declare -a subjects=(
- #
-#  "Eclipse_jetty_1_eps1_1"
-#  "leaksn1b-5_1"
-#  "blazer_modpow1_unsafe"
-#  "blazer_modpow2_unsafe"
-#  "blazer_k96_unsafe"
-#  "blazer_gpt14_unsafe"
-#  "blazer_login_unsafe"
+ "Eclipse_jetty_1_eps1_1"
+ "leaksn1b-5_1"
+ "blazer_loopandbranch_unsafe"
+ "blazer_modpow1_unsafe"
+ "blazer_modpow2_unsafe"
+ "blazer_k96_unsafe"
+ "blazer_gpt14_unsafe"
+ "blazer_login_unsafe"
  "blazer_unixlogin_unsafe"
- "blazer_sanity_unsafe"
- "themis_jdk_unsafe"
  "leaksn1b-1_1"
  )
 
