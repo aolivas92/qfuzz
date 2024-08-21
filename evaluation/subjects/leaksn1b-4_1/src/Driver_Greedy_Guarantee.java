e55e343033a77b3179eb1b916c8b6bcea52cbc5e
root <root@ip-172-31-7-46.us-east-2.compute.internal>
2024-08-21 22:42:50 +0000

    table 1 round 2

--- evaluation/subjects/leaksn1b-4_1/src/Driver_Greedy_Guarantee.java
+++ evaluation/subjects/leaksn1b-4_1/src/Driver_Greedy_Guarantee.java
@@ -87,7 +87,7 @@ public class Driver_Greedy_Guarantee {
 
         Kelinci.addCost(analyticsLong);
 
-        String dirPath = "./log/log_30min_a/";
+        String dirPath = "./log/log_30min_b/";
 
         // Log Everything
         String logPath = "Log.txt";
