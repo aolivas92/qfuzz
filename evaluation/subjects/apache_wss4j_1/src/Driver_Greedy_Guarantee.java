f47a9820c143dd1c2b639a252524839975a8fa6d
root <root@ip-172-31-7-46.us-east-2.compute.internal>
2024-09-09 22:41:55 +0000

    apache run - graybox

---
+++ evaluation/subjects/apache_wss4j_1/src/Driver_Greedy_Guarantee.java
@@ -0,0 +1,291 @@
+import org.apache.ws.security.WSSecurityException;
+import org.apache.wss4j.binding.wss10.PasswordString;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import edu.cmu.sv.kelinci.Kelinci;
+import edu.cmu.sv.kelinci.Mem;
+import edu.cmu.sv.kelinci.quantification.PartitionAlgorithm;
+import edu.cmu.sv.kelinci.quantification.PartitionSet;
+import edu.cmu.sv.kelinci.quantification.Greedy;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.NumberFormatException;
+import java.io.PrintWriter;
+
+
+public class Driver_Greedy_Guarantee {
+
+    /* Maximum number of different observations. */
+    public final static int K = 2;
+
+    /* Minimum distance between clusters. */
+    public final static double epsilon = 1.0;
+
+    /* Cluster Algorithm */
+    public static PartitionAlgorithm clusterAlgorithm = new Greedy(false);
+
+    ////////////////////////////////////////////////////////////////////////
+
+    public static final int MAX_PASSWORD_LENGTH = 32; // bytes
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Expects file name as parameter");
+            return;
+        }
+
+        String validPassword_public;
+        String[] storedPassword_secret = new String[K];
+
+        // Read all inputs.
+        try (FileInputStream fis = new FileInputStream(args[0])) {
+
+            /* Read all data. */
+            int i = 0;
+            int value;
+            List<Character> values = new ArrayList<>();
+            while (((value = fis.read()) != -1) && (i < (K + 1) * MAX_PASSWORD_LENGTH)) {
+                /* each char value must be between 0 and 127 and a printable character */
+                value = value % 127;
+                char charValue = (char) value;
+                if (Character.isAlphabetic(charValue) || Character.isDigit(charValue)) {
+                    values.add(charValue);
+                    i++;
+                }
+            }
+            /* input must be non-empty */
+            int eachSize = values.size() / (K + 1);
+            if (eachSize % Character.BYTES == 1) {
+                eachSize--;
+            }
+            if (eachSize < 1) {
+                throw new RuntimeException("not enough data!");
+            }
+            System.out.println("eachSize=" + eachSize);
+
+            char[] tmp_array = new char[eachSize];
+            for (i = 0; i < eachSize; i++) {
+                tmp_array[i] = values.get(i);
+            }
+            validPassword_public = new String(tmp_array);
+
+            for (int j = 0; j < K; j++) {
+                tmp_array = new char[eachSize];
+                for (i = 0; i < eachSize; i++) {
+                    tmp_array[i] = values.get(i + (j + 1) * eachSize);
+                }
+                storedPassword_secret[j] = new String(tmp_array);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading input");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("password=" + validPassword_public);
+        for (int i = 0; i < K; i++) {
+            System.out.println("secret[" + i + "]=" + storedPassword_secret[i]);
+        }
+
+        PasswordString passwordType = new PasswordString();
+        passwordType.setValue(validPassword_public);
+
+        long[] observations = new long[K];
+        Mem.clear(true);
+        for (int i = 0; i < K; i++) {
+            Mem.clear(false);
+            try {
+                SimplifiedUsernameTokenValidatorImpl.verifyPlaintextPassword(passwordType, storedPassword_secret[i]);
+            } catch (WSSecurityException e) {
+            }
+            observations[i] = Mem.instrCost;
+        }
+        System.out.println("observations: " + Arrays.toString(observations));
+
+        // Start of research
+        // Calculate analytics
+        double analytics = Math.abs(observations[0] - observations[1]);
+        long analyticsLong = (long) analytics;
+
+        Kelinci.addCost(analyticsLong);
+
+        String dirPath = "./log/log_60min_a/";
+
+        // Log Everything
+        String logPath = "Log.txt";
+        String data = Double.toString(analytics);
+        writeToLog(logPath, data, dirPath, true);
+
+        // Read Everything from Unique file
+        String uniqueLogPath = "Unique_Log.txt";
+        SortedSet<Double> uniqueValues = readDoubleSetLog(dirPath + uniqueLogPath);
+
+        // Read Test Log file
+        String countLog = "Count_Log.txt";
+        Long count = readLongLog(dirPath + countLog);
+        count += 1;
+        writeToLog(countLog, Long.toString(count), dirPath, false);
+
+        int min_num_tail = 15;
+        int threshold = 10;
+
+        String testPassedLog = "Test_Passed_Log.txt";
+
+        // Unique Value Test
+        if (!uniqueValues.contains(analytics)) {
+            uniqueValues.add(analytics);
+            writeToLog(uniqueLogPath, Double.toString(analytics), dirPath, true);
+
+            // If we found a new unique value, we have more than 15 unique values, and the
+            // size of unique values is divisble by 5, try the Exponential Test.
+            if (uniqueValues.size() >= min_num_tail && uniqueValues.size() % 5 == 0 && expTest(threshold, uniqueValues) > 0) {
+                writeToLog(logPath, "-1", dirPath, true);
+
+                long currTime = System.nanoTime();
+                String time = String.valueOf(currTime);
+                String testInfo = count + " " + uniqueValues.size() + " " + time;
+                writeToLog(testPassedLog, testInfo, dirPath, true);
+
+            }
+        }
+
+        System.out.println("Done.");
+    }
+
+    /* Logging Algorithm */
+    public static void writeToLog(String fileName, String data, String dirPath, Boolean append) {
+        try {
+            // Check if dir exists
+            // Folder Path
+            File directory = new File(dirPath);
+            if (!directory.exists()) {
+                directory.mkdirs();
+                long startTime = System.nanoTime();
+                String time = "0 " + "0 " + String.valueOf(startTime);
+                writeToLog("Test_Passed_Log.txt", time, dirPath, false);
+            }
+
+            // Write to log file
+            fileName = dirPath + fileName;
+            FileWriter writer = new FileWriter(fileName, append);
+            PrintWriter out = new PrintWriter(writer);
+
+            out.println(data);
+
+            out.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Read Log File with a Set of Doubles */
+    public static SortedSet<Double> readDoubleSetLog(String fileName) {
+        SortedSet<Double> doubleSet = new TreeSet<>();
+
+        File file = new File(fileName);
+
+        try {
+            // If file doesn't exists, create one and return empty set
+            if (!file.exists()) {
+                file.createNewFile();
+                return doubleSet;
+            }
+
+            // Read file if it exists
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    double value = Double.parseDouble(line.trim());
+                    doubleSet.add(value);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return doubleSet;
+    }
+
+    /* Read Log File with Long values */
+    public static long readLongLog(String fileName) {
+        File file = new File(fileName);
+        Long num = (long) -1;
+
+        try {
+            // If file doesn't exists, create one and return empty set
+            if (!file.exists()) {
+                file.createNewFile();
+                // testLog: total count, if test passed, locationg passed, count after passed,
+                // num Unique samples when passed.
+                return 0;
+            }
+
+            // Read file if it exists
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                num = Long.parseLong(line);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return num;
+    }
+
+    /* Exponential Test */
+    public static Integer expTest(int threshold, SortedSet<Double> sortedSet) {
+        int maxNumTailSamples = Math.min(sortedSet.size(), 50);
+
+        // NumTailSamples will be at most will be 50.
+        for (int j = threshold; j <= maxNumTailSamples; j++) {
+            // Sorted list of the cost difference that starts at j-1 to the end.
+            List<Double> xTail = new ArrayList<>(sortedSet).subList(sortedSet.size() - j, sortedSet.size());
+
+            // Do the exponential testing
+            double m = xTail.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+            double st = Math.sqrt(xTail.stream().mapToDouble(val -> Math.pow(val - m, 2)).average().orElse(0));
+            double cv = 0;
+            if (m == 0) {
+                cv = st / m;
+                continue;
+            }
+
+            // If the cv is greater than 1 then break.
+            if (cv > 1) {
+                return -1;
+            }
+            // If j gets to the last sample then exp test has passed and return true.
+            if (j == (maxNumTailSamples - threshold)) {
+                return maxNumTailSamples;
+            }
+        }
+        return -1; // Default return value
+    }
+}
+
+
+
+
