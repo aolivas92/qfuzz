35e1c5883152281fd067949e084b6b5f951b8c0d
adhfmz8 <73725651+adhfmz8@users.noreply.github.com>
2025-01-14 12:56:12 -0700

    output.csv files

--- evaluation/scripts/research/logToCSV.py
+++ evaluation/scripts/research/logToCSV.py
@@ -2,18 +2,40 @@ import pandas as pd
 import os
 import subprocess
 
-log_dir = "/log/log_30min_2/"
+# log_dir = "/log/log_30min_2/"
 output_file = "output.csv"
 
-subjects=[
-"leaksn1b-5_1"
-]
+# Path to the subjects folder
+subjects_dir = '../../../evaluation/subjects'
 
-for subject in subjects:
-    cur_subject = "../../subjects/" + subject + log_dir
+# List to store paths to subfolders containing 'Test_Passed_log.txt'
+log_folders_with_file = []
+
+# Traverse the subjects folder
+for subject_name in os.listdir(subjects_dir):
+    subject_path = os.path.join(subjects_dir, subject_name)
+
+    if os.path.isdir(subject_path):  # Check if it's a directory
+        log_path = os.path.join(subject_path, 'log')
+
+        if os.path.isdir(log_path):  # Check if the 'log' folder exists
+            # Traverse the log folder
+            for log_subfolder in os.listdir(log_path):
+                log_subfolder_path = os.path.join(log_path, log_subfolder)
+
+                if os.path.isdir(log_subfolder_path):  # Check if it's a subfolder
+                    # Check if 'Test_Passed_log.txt' exists
+                    log_file_path = os.path.join(log_subfolder_path, 'Test_Passed_log.txt')
+                    if os.path.isfile(log_file_path):
+                        log_folders_with_file.append(log_subfolder_path + '/')
+
+
+print(log_folders_with_file[0])
+for cur_subject in log_folders_with_file:
+    # cur_subject = "../../subjects/" + subject + log_dir
 
     if not os.path.exists(cur_subject + "Test_Passed_Log.txt"):
-        print("Test_Passed_Log.txt doesnt exist for:", subject)
+        print("Test_Passed_Log.txt doesnt exist for:", cur_subject)
         continue
 
     with open(cur_subject + 'Log.txt', 'r') as file:
@@ -38,4 +60,4 @@ for subject in subjects:
     le_data = le_data.transpose()
     le_data.to_csv(cur_subject + output_file)
 
-    print("Finished:", subject)
+    print("Finished:", cur_subject)
